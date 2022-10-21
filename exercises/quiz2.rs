c970a199260dcb1b3fc35917a94e7f311d55a53f
Claudiu Guiman <clguiman@microsoft.com>
2022-10-21 16:41:12 -0400

    strings, hashmaps and quiz 2

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,8 +18,6 @@
 // - The output element is going to be a Vector of strings.
 // Execute `rustlings hint quiz2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -27,23 +25,38 @@ pub enum Command {
 }
 
 mod my_module {
+    //use std::process::Command;
+
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(&str, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            let mut transformed_value: String = string.to_string();
+            match command {
+                Command::Uppercase => transformed_value = transformed_value.to_uppercase(),
+                Command::Trim => transformed_value = transformed_value.trim().to_string(),
+                Command::Append(times) => append_bar(&mut transformed_value, *times),
+            }
+            output.push(transformed_value);
         }
         output
     }
+
+    fn append_bar(string: &mut String, times: usize) {
+        for _ in 0..times {
+            string.push_str("bar");
+        }
+    }
+
 }
 
 #[cfg(test)]
 mod tests {
     // TODO: What do we have to import to have `transformer` in scope?
-    use ???;
+    use my_module::transformer;
     use super::Command;
 
     #[test]
